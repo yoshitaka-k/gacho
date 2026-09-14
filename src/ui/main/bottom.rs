e836dafb4db7f7996d567a8afe7c37d67d482225
Yoshitaka <yoshitaka@ktgwya.jp>
2026-09-14 18:16:16 +0900

    refactor: layout

--- src/ui/main/bottom.rs
+++ src/ui/main/bottom.rs
@@ -60,64 +60,61 @@ pub(crate) fn view(
                 ui.add(egui::Label::new(file_names.join("")).truncate());
             },
             |ui| {
-                // ページャーを右寄せに配置
-                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                    // 前のファイルボタン
-                    let hover_text = match app.read_from() {
-                        event::ReadFrom::RightToLeft => "Previous page",
-                        event::ReadFrom::LeftToRight => "Next page",
+                // 前のファイルボタン
+                let hover_text = match app.read_from() {
+                    event::ReadFrom::RightToLeft => "Previous page",
+                    event::ReadFrom::LeftToRight => "Next page",
+                };
+                let prev_button = egui::Image::new(svg::KEYBOARD_ARROW_RIGHT)
+                    .tint(button_color);
+                if ui.add_sized(icon::ICON_BUTTON_SIZE, egui::Button::image(prev_button))
+                    .on_hover_text(hover_text).clicked()
+                {
+                    pending_actions.push(event::EventAction::Right);
+                }
+
+                // 次のファイルボタン
+                let hover_text = match app.read_from() {
+                    event::ReadFrom::RightToLeft => "Next page",
+                    event::ReadFrom::LeftToRight => "Previous page",
+                };
+                let next_button = egui::Image::new(svg::KEYBOARD_ARROW_LEFT)
+                    .tint(button_color);
+                if ui.add_sized(icon::ICON_BUTTON_SIZE, egui::Button::image(next_button))
+                    .on_hover_text(hover_text).clicked()
+                {
+                    pending_actions.push(event::EventAction::Left);
+                }
+
+                ui.separator();
+
+                // ページャースライダー
+                let mut selected = open_files.page_mut().unwrap_or(0);
+                let max = if open_files.book_len() > 0 { open_files.book_len() - 1 } else { DEFAULT_MAX_INDEX };
+                ui.scope(|ui| {
+                    ui.spacing_mut().slider_width = SLIDER_WIDTH;
+                    let slider = match app.read_from() {
+                        event::ReadFrom::RightToLeft => ui.add(
+                            egui::Slider::new(&mut selected, max..=MIN_INDEX)
+                            .show_value(false)
+                        ),
+                        event::ReadFrom::LeftToRight => ui.add(
+                            egui::Slider::new(&mut selected, MIN_INDEX..=max)
+                            .show_value(false)
+                        ),
                     };
-                    let prev_button = egui::Image::new(svg::KEYBOARD_ARROW_RIGHT)
-                        .tint(button_color);
-                    if ui.add_sized(icon::ICON_BUTTON_SIZE, egui::Button::image(prev_button))
-                        .on_hover_text(hover_text).clicked()
-                    {
-                        pending_actions.push(event::EventAction::Right);
+
+                    // 選択が変更された場合はインデックスを更新
+                    if slider.changed() {
+                        // エラーモーダルをリセット
+                        error_token.reset();
+
+                        // インデックスを更新
+                        open_files.set_page(Some(selected));
                     }
-
-                    // 次のファイルボタン
-                    let hover_text = match app.read_from() {
-                        event::ReadFrom::RightToLeft => "Next page",
-                        event::ReadFrom::LeftToRight => "Previous page",
-                    };
-                    let next_button = egui::Image::new(svg::KEYBOARD_ARROW_LEFT)
-                        .tint(button_color);
-                    if ui.add_sized(icon::ICON_BUTTON_SIZE, egui::Button::image(next_button))
-                        .on_hover_text(hover_text).clicked()
-                    {
-                        pending_actions.push(event::EventAction::Left);
-                    }
-
-                    ui.separator();
-
-                    // ページャースライダー
-                    let mut selected = open_files.page_mut().unwrap_or(0);
-                    let max = if open_files.book_len() > 0 { open_files.book_len() - 1 } else { DEFAULT_MAX_INDEX };
-                    ui.scope(|ui| {
-                        ui.spacing_mut().slider_width = SLIDER_WIDTH;
-                        let slider = match app.read_from() {
-                            event::ReadFrom::RightToLeft => ui.add(
-                                egui::Slider::new(&mut selected, max..=MIN_INDEX)
-                                .show_value(false)
-                            ),
-                            event::ReadFrom::LeftToRight => ui.add(
-                                egui::Slider::new(&mut selected, MIN_INDEX..=max)
-                                .show_value(false)
-                            ),
-                        };
-
-                        // 選択が変更された場合はインデックスを更新
-                        if slider.changed() {
-                            // エラーモーダルをリセット
-                            error_token.reset();
-
-                            // インデックスを更新
-                            open_files.set_page(Some(selected));
-                        }
-                    });
-
-                    ui.separator();
                 });
+
+                ui.separator();
             }
         );
     });
